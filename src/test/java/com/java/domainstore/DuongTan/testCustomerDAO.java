16d1fe762ada030965f9b5758ce2246a534ccf75
AuDuongTan <6451071069@st.utc2.edu.vn>
2025-03-12 01:09:07 +0700

    Lưu thay đổi trước khi revert

--- src/test/java/com/java/domainstore/DuongTan/testCustomerDAO.java
+++ src/test/java/com/java/domainstore/DuongTan/testCustomerDAO.java
@@ -1,19 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.java.domainstore.DuongTan;
 
+import com.java.domainstore.repository.JDBC;
 import com.java.domainstore.dao.CustomerDAO;
-import com.java.domainstore.model.Customer;
-import com.java.domainstore.model.Customer.Role;
 import java.sql.Date;
+import com.java.domainstore.model.Customer;
 import java.util.ArrayList;
 
 public class testCustomerDAO {
     public static void main(String[] args) {
-        CustomerDAO customerDAO = CustomerDAO.getInstance();
+        //them
+//        Customer kh1 = new Customer("KH006", "Trịnh Trần Phương Tuấn", Date.valueOf("1997-03-03"), "082265718564", "Bến Tre", "ttpt1997@gmail.com", "0327876533", "iJtVkPfPZGpsIDHlre5bL/FzRiePU5JZmdzyWETsZx4=", Customer.Role.user);
+//        CustomerDAO.getInstance().insertCustomer(kh1);
+        
+        //xoa
+//        Customer kh1 = new Customer("KH006", "Trịnh Trần Phương Tuấn", Date.valueOf("1997-03-03"), "082265718564", "Bến Tre", "ttpt1997@gmail.com", "0327876533", "pass03031998", Customer.Role.user);
+//        CustomerDAO.getInstance().deleteCustomer(kh1.getId());
 
-        // Thêm khách hàng mới
-//        Customer newCustomer = new Customer("KH009", "Trịnh Trần Phương Tuấn", Date.valueOf("1997-03-03"), "082265718564", "Bến Tre", "ttpt1997@gmail.com", "0327876533", "password123", "", Role.user);
-//        int insertResult = customerDAO.insert(newCustomer);
-//        System.out.println(insertResult > 0 ? "Thêm khách hàng thành công!" : "Thêm khách hàng thất bại!");
+        //update
+//        Customer kh1 = new Customer("KH006", "Trịnh Trần Phương Tuấn", Date.valueOf("1997-03-03"), "082265718564", "Bến Tre", "ttpt1997@gmail.com", "0327876533", "pass012312", Customer.Role.user);
+//        CustomerDAO.getInstance().updateCustomer(kh1);
 
         // Lấy danh sách tất cả khách hàng
 //        ArrayList<Customer> customers = customerDAO.selectAll();
@@ -38,4 +47,5 @@ public class testCustomerDAO {
 //        Customer newCustomer = new Customer("KH006", "Trịnh Trần Phương Tuấn", Date.valueOf("1997-03-03"), "082265718564", "Bến Tre", "ttpt1997@gmail.com", "0327876533", "password123", "", Role.user);
 //        int deleteResult = customerDAO.delete(newCustomer);
     }
+    
 }
